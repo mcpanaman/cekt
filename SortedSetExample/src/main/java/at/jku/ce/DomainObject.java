0b435d5804795dfa4d8f71843a7e2f458d4b15b2
pHelmberger <peter.helmberger@hotmail.com>
2012-11-27 13:54:05 +0100

    <Tag 8> Barbar: override methood equals

--- SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
@@ -26,4 +26,12 @@ public class DomainObject {
 			", comment=" + comment 
            + ", uuid=" + uuid + "]"; 
 	} 
+	
+	public boolean equals(Object obj) { 
+		if (!(obj instanceof DomainObject)) { 
+			return false; 
+		} 
+		DomainObject domainObj = (DomainObject) obj; 
+		return uuid.equals(domainObj.getUuid()); 
+	} 
 }
